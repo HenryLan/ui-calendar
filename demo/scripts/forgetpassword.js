26572548297e27d68d43adf81f573acacf9d25a6
henrylanca <henrylanca@gmail.com>
2015-05-14 11:38:38 -0400

    20150514_1138

---
+++ demo/scripts/forgetpassword.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    var app = angular.module('demoApp');
+
+    var forgetpasswordCtrl = function ($scope, dataSvc) {
+        $scope.email = "";
+		$scope.info = "";
+		//$scope.resetPassword = resetPassword;
+		$scope.getPasword = getPasword;
+
+/*         function resetPassword() {
+			alert('ok');
+        }; */
+		
+		function getPasword(){
+ 			dataSvc.resetPassword($scope.email)
+			.success(function (data, status, headers, config) {
+				$scope.info = "Sent Reset password email";
+			})
+			.error(function (data, status, headers, config) {
+				$scope.info = "Please contact administrator";
+			}); 
+		};
+    };
+
+    app.controller("forgetpasswordCtrl", ["$scope", "dataSvc", forgetpasswordCtrl]);
+}());
